79768ecd01830093ae6c9ec403c274cd7ec4c0f8
Iverson <iversonwang40@gmail.com>
2024-05-10 14:36:06 +0800

    feat: sign in spinner

---
+++ src/components/little-spinner.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 flex justify-center"
+  >
+    <span class="spinner"></span>
+    <span class="ml-3">Processing</span>
+  </div>
+</template>
+<style>
+.spinner {
+  /* Spinner size and color */
+  width: 1.5rem;
+  height: 1.5rem;
+  border-top-color: #fff;
+  border-left-color: #fff;
+
+  /* Additional spinner styles */
+  animation: spinner 800ms linear infinite;
+  border-bottom-color: transparent;
+  border-right-color: transparent;
+  border-style: solid;
+  border-width: 2px;
+  border-radius: 50%;
+  box-sizing: border-box;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+@keyframes spinner {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -107,8 +107,9 @@
           </div>
 
           <div class="mt-6">
+            <LittleSpinner v-if="isProcessing" />
             <button
-              :disabled="isProcessing"
+              v-else
               class="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
             >
               Sign In
@@ -135,8 +136,13 @@
 <script>
 import authorizationAPI from './../apis/authorization'
 import { Toast } from './../utils/helpers'
+import LittleSpinner from './../components/little-spinner'
 
 export default {
+  name: 'SignIn',
+  components: {
+    LittleSpinner
+  },
   data() {
     return {
       email: '',
