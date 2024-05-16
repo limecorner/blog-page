4ccb2729be1b7194ac6b51baa7f680ddc62462a5
Iverson <iversonwang40@gmail.com>
2024-05-16 16:51:36 +0800

    feat: edit, delete response only for user self

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -143,22 +143,27 @@
               <h1 class="text-2xl text-gray-700 hover:underline">
                 {{ content }}
               </h1>
-              <button
-                @click="
-                  dialogVisible = true
-                  editResponseId = id
-                  reviseForm.content = content
-                "
-                class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 mr-5"
+              <div
+                v-show="currentUser.id === User.id"
+                class="flex justify-end items-center mt-4"
               >
-                編輯
-              </button>
-              <button
-                @click.stop.prevent="removeReponse(id, content)"
-                class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-600 rounded-lg hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80"
-              >
-                刪除
-              </button>
+                <button
+                  @click="
+                    dialogVisible = true
+                    editResponseId = id
+                    reviseForm.content = content
+                  "
+                  class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 mr-5"
+                >
+                  編輯
+                </button>
+                <button
+                  @click.stop.prevent="removeReponse(id, content)"
+                  class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-600 rounded-lg hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80"
+                >
+                  刪除
+                </button>
+              </div>
             </div>
           </div>
         </div>
