faa7063669b1b024abb378955f041aa20dc59766
Iverson <iversonwang40@gmail.com>
2024-05-17 13:16:24 +0800

    UI: user page

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,3 +1,85 @@
 <template>
-  <h1>Users.vue</h1>
+  <div class="font-roboto bg-gray-100">
+    <div class="px-6 py-8">
+      <div class="flex justify-between container mx-auto">
+        <div class="w-full lg:w-8/12">
+          <div class="flex items-center justify-between">
+            <h1 class="text-xl font-bold text-gray-700 md:text-2xl">
+              {{ currentUser.name }}
+            </h1>
+            <post-filter></post-filter>
+          </div>
+          <div class="post-container">
+            <div class="mt-6" v-for="post in posts" :key="post.id">
+              <post :data="post"></post>
+            </div>
+          </div>
+        </div>
+        <div class="-mx-8 w-4/12 hidden lg:block">
+          <div class="px-8">
+            <h1 class="mb-4 text-xl font-bold text-gray-700">
+              {{ currentUser.name }}
+            </h1>
+            <p>{{ currentUser.bio }} Followers</p>
+            <p>{{ currentUser.bio }}</p>
+          </div>
+          <div class="mt-10 px-8">
+            <h1 class="mb-4 text-xl font-bold text-gray-700">Following</h1>
+            <users-list></users-list>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import PostFilter from './../components/elements-select-option.vue'
+import Post from './../components/elements-blog-post-article-review.vue'
+import UsersList from './../components/sections-blog-users-list.vue'
+
+import articlesAPI from './../apis/articles'
+// import responsesAPI from './../apis/responses'
+import { Toast } from './../utils/helpers'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'User',
+  components: {
+    PostFilter,
+    Post,
+    UsersList
+  },
+  data() {
+    return {
+      posts: []
+    }
+  },
+  methods: {
+    async fetchArticles() {
+      try {
+        this.isLoading = true
+
+        const { data } = await articlesAPI.getArticles()
+        if (data.success === true) {
+          this.posts = data.articles
+        }
+
+        this.isLoading = false
+      } catch (error) {
+        this.isLoading = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得文章，請稍後再試'
+        })
+      }
+    }
+  },
+  created() {
+    this.fetchArticles()
+  },
+  computed: {
+    ...mapState(['currentUser', 'isAuthenticated'])
+  }
+}
+</script>
