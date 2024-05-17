daa4b06c3bf0724f3e106d5c1f909107ad0bfa30
Iverson <iversonwang40@gmail.com>
2024-05-17 17:09:11 +0800

    feat: removeIdol

--- src/components/sections-blog-users-list.vue
+++ src/components/sections-blog-users-list.vue
@@ -14,6 +14,7 @@
             data.name
           }}</a>
           <button
+            @click.stop.prevent="removeIdol(data.id)"
             class="ml-3 px-2 py-1 font-medium tracking-wide text-green-500 capitalize transition-colors duration-300 transform  border border-green-500 rounded-lg hover:border-green-700 hover:text-green-700"
           >
             Following
@@ -25,7 +26,36 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import followshipsAPI from './../apis/followships'
+import { Toast } from './../utils/helpers'
+
 export default {
-  props: ['data']
+  props: ['data'],
+  methods: {
+    async removeIdol(id) {
+      try {
+        const { data } = await followshipsAPI.deleteIdol(id)
+        if (data.success === true) {
+          this.currentUser.Idols = this.currentUser.Idols.filter(
+            (idol) => idol.id !== id
+          )
+          Toast.fire({
+            icon: 'success',
+            title: '成功取消追蹤此偶像'
+          })
+        }
+      } catch (error) {
+        const errorMessage = error.response.data.message
+        Toast.fire({
+          icon: 'error',
+          title: errorMessage ? errorMessage : '無法取消追蹤此偶像，請稍後再試'
+        })
+      }
+    }
+  },
+  computed: {
+    ...mapState(['currentUser'])
+  }
 }
 </script>
