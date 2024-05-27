f74f49d0c5773efbba3416d077327022c0af6558
Iverson <iversonwang40@gmail.com>
2024-05-27 17:52:32 +0800

    api: getUser

--- src/apis/users.js
+++ src/apis/users.js
@@ -3,5 +3,8 @@ import { apiHelper } from './../utils/helpers'
 export default {
   getCurrentUser() {
     return apiHelper.get('/users/current-user')
+  },
+  getUser(id) {
+    return apiHelper.get(`/users/${id}`)
   }
 }
